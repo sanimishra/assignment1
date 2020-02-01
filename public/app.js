2d17e48449e5198bd09ccfef3f1f2344f99cc54f
sanimishra <30474014+sanimishra@users.noreply.github.com>
2020-01-31 17:51:31 -0800

    first project changes

---
+++ public/app.js
@@ -0,0 +1,7 @@
+const element = React.createElement("div", null, React.createElement("h1", null, "sani"), React.createElement("img", {
+  src: "images/sani.jpg",
+  alt: "mypicture"
+}), React.createElement("p", null, "I am Sani Mishra.I am currently pursuing master's from San diego State University in Computer Science.I enjoy cooking.I am also very passionate about music.So I love to sing using various karaoke app."), React.createElement("a", {
+  href: "link.html"
+}, "View my Github repo"));
+ReactDOM.render(element, document.getElementById('contents'));
---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+const element = (
+  <div>
+    <h1>sani</h1>
+    <img src="images/sani.jpg" alt="mypicture"/>
+    <p>
+     I am Sani Mishra.I am currently pursuing master's from San diego State 
+     University in Computer Science.I enjoy cooking.I am also very passionate about music.So I love to sing using various karaoke app.
+    </p>
+    <a href="link.html">View my Github repo</a>
+  </div>
+  );
+  ReactDOM.render(element, document.getElementById('contents'));
