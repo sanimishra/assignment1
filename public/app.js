e896977efff90abcb17c1d9cfa96775a6fb64e85
sanimishra <30474014+sanimishra@users.noreply.github.com>
2020-01-31 17:55:25 -0800

    previous link removed

--- public/app.js
+++ public/app.js
@@ -2,6 +2,6 @@ const element = React.createElement("div", null, React.createElement("h1", null,
   src: "images/sani.jpg",
   alt: "mypicture"
 }), React.createElement("p", null, "I am Sani Mishra.I am currently pursuing master's from San diego State University in Computer Science.I enjoy cooking.I am also very passionate about music.So I love to sing using various karaoke app."), React.createElement("a", {
-  href: "link.html"
+  href: ""
 }, "View my Github repo"));
 ReactDOM.render(element, document.getElementById('contents'));
--- src/app.jsx
+++ src/app.jsx
@@ -6,7 +6,7 @@ const element = (
      I am Sani Mishra.I am currently pursuing master's from San diego State 
      University in Computer Science.I enjoy cooking.I am also very passionate about music.So I love to sing using various karaoke app.
     </p>
-    <a href="link.html">View my Github repo</a>
+    <a href="">View my Github repo</a>
   </div>
   );
   ReactDOM.render(element, document.getElementById('contents'));
